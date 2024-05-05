c75c93c5244879230fc092d772ec077c54800219
TimofeyKibardin <timoshka.kartoshka99@gmail.com>
2024-05-05 12:09:13 +0300

    Задача будет воспроизводиться каждые 2 часа

--- lib/actions/cronJob.ts
+++ lib/actions/cronJob.ts
@@ -3,8 +3,9 @@ import { GET } from '@/app/api/cron/route';
 
 let cronLock = false;
 
+//Обновление каждые 2 часа
 export const cronJob = new CronJob(
-	'0 */5 * * * *',
+	'0 0 */2 * * *',
 	function() {
 		console.log('cronLock: ' + cronLock);
 		if (cronLock) return;
